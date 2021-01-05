b914801ac2d94cedf45c3ee77ea15e7dec73778c
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-01-05 16:00:28 +0900

    [fix]ft_strcmp.s, Supports hexadecimal numbers

--- main.c
+++ main.c
@@ -1,117 +1,44 @@
 #include "./includes/libasm.h"
 
-void	print_ft_strlen(char s[]){
-	int n;
-	printf("-----ft_strlen(%s)-----\n", s);
-	n = ft_strlen(s);
-	printf("ft_strlen:[%d]\n", n);
-	n = strlen(s);
-	printf("   strlen:[%d]\n\n", n);
-}
-
-void	print_ft_strcpy(char s1[], char s2[]){
-	char *tmp = strdup(s1);
-	char *s3;
-	printf("-----ft_strcpy(%s, %s)-----\n", s1, s2);
-	s3 = ft_strcpy(s1, s2);
-	printf("ft_strcpy:[%s]\n", s3);
-	ft_strcpy(s1, tmp);
-	s3 = strcpy(s1, s2);
-	printf("   strcpy:[%s]\n\n", s3);
-	strcpy(s1, tmp);
-	free(tmp);
-}
-
-void	print_ft_strcmp(char s1[], char s2[]){
-	int n;
-	printf("-----ft_strcmp(%s, %s)-----\n", s1, s2);
-	n = ft_strcmp(s1, s2);
-	printf("ft_strcmp:[%d]\n", n);
-	n = strcmp(s1, s2);
-	printf("   strcmp:[%d]\n\n", n);
-}
-
-void	print_ft_write(int fd, char s[], size_t byte){
-	errno = 0;
-	printf("-----ft_write(%d, %s, %zu)-----\n", fd, s, byte);
-	printf("ft_write:[");
-	int count = ft_write(fd, s, byte);
-	printf("] count:[%d]", count);
-	printf(" strerror:[%s]", strerror(errno));
-	printf(" error:[%d]\n", errno);
-	errno = 0;
-	printf("   write:[");
-	count = write(fd, s, byte);
-	printf("] count:[%d]", count);
-	printf(" strerror:[%s]", strerror(errno));
-	printf(" error:[%d]\n\n", errno);
-}
-
-void	print_ft_read(char pass[], char buf[]){
-	int fd;
-	int n;
-	int i = -1;
-	while (++i < 1024)
-		buf[i] = 0;
-	errno = 0;
-	fd = open(pass, O_RDONLY);
-	n = ft_read(fd, buf, 1024);
-	printf("-----ft_read(fd:[%d], pass:[%s])-----\n", fd, pass);
-	printf("ft_read:[%s]\n", buf);
-	printf("val:[%d], strerror:[%s], errno:[%d]\n", n, strerror(errno), errno);
-	close(fd);
-	i = 0;
-	while (++i < 1024)
-		buf[i] = 0;
-	errno = 0;
-	fd = open(pass, O_RDONLY);
-	n = read(fd, buf, 1024);
-	printf("   read:[%s]\n", buf);
-	printf("val:[%d], strerror:[%s], errno:[%d]\n\n", n, strerror(errno), errno);
-	close(fd);
-}
-
-void	print_ft_strdup(char s[]){
-	char *dup;
-
-	printf("-----ft_strdup(%s)-----\n", s);
-	dup = ft_strdup(s);
-	printf("ft_strdup:[%s]\n", dup);
-	free(dup);
-	dup = strdup(s);
-	printf("   strdup:[%s]\n\n", dup);
-	free(dup);
-}
-
 int main()
 {
     printf("------------ strlen -------------\n");
-    printf("[honke: %zu], [zisaku: %zu]\n", strlen("hello"), ft_strlen("hello"));
-    printf("[honke: %zu], [zisaku: %zu]\n", strlen("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), ft_strlen("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-    printf("[honke: %zu], [zisaku: %zu]\n", strlen(""), ft_strlen(""));
+    printf("[honke: %zu], [zisaku: %zu]\n\n", strlen("hello"), ft_strlen("hello"));
+    printf("[honke: %zu], [zisaku: %zu]\n\n", strlen("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), ft_strlen("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+    printf("[honke: %zu], [zisaku: %zu]\n\n", strlen(""), ft_strlen(""));
 
     printf("------------ strcpy -------------\n");
     char    cpy1[100] = "abc";
     char    cpy2[100] = "abc";
 
+    char    *tmp1;
+    char    *tmp2;
+    tmp1 = strcpy(cpy1, "aaa");
+    tmp2 = ft_strcpy(cpy2, "aaa");
+    printf("[honke: %s], [zisaku: %s]\n\n", tmp1, tmp2);
+
     strcpy(cpy1, "");
     ft_strcpy(cpy2, "");
-    printf("[honke: %s], [zisaku: %s]\n", cpy1, cpy2);
+    printf("[honke: %s], [zisaku: %s]\n\n", cpy1, cpy2);
     strcpy(cpy1, "abcdefg");
     ft_strcpy(cpy2, "abcdefg");
-    printf("[honke: %s], [zisaku: %s]\n", cpy1, cpy2);
+    printf("[honke: %s], [zisaku: %s]\n\n", cpy1, cpy2);
     strcpy(cpy1, "1");
-    ft_strcpy(cpy2, "1");    
-    printf("[honke: %s], [zisaku: %s]\n", cpy1, cpy2);
+    ft_strcpy(cpy2, "1");
+    printf("[honke: %s], [zisaku: %s]\n\n", cpy1, cpy2);
 
     printf("------------ strcmp -------------\n");
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("abc", "abc"), ft_strcmp("abc", "abc"));
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("abc", "abb"), ft_strcmp("abc", "abb"));
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("abb", "abc"), ft_strcmp("abb", "abc"));
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("abc", ""), ft_strcmp("abc", ""));
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("", "abc"), ft_strcmp("", "abc"));
-    printf("[honke: %d], [zisaku: %d]\n", strcmp("", ""), ft_strcmp("", ""));
-
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("abc", "abc"), ft_strcmp("abc", "abc"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("abc", "abb"), ft_strcmp("abc", "abb"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("abb", "abc"), ft_strcmp("abb", "abc"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("abc", ""), ft_strcmp("abc", ""));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("", "abc"), ft_strcmp("", "abc"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("", ""), ft_strcmp("", ""));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("\xff\xff", "\xff"), ft_strcmp("\xff\xff", "\xff"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("\xff", ""), ft_strcmp("\xff", ""));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("\xff", "\xff\xff"), ft_strcmp("\xff", "\xff\xff"));
+    printf("[honke: %d], [zisaku: %d]\n\n", strcmp("\xfe", "\xfe\xff"), ft_strcmp("\xfe", "\xfe\xff"));
+    
     printf("------------ write -------------\n");
 	int     count;
     
@@ -210,14 +137,14 @@ int main()
     char    *dup2;
 
     dup1 = strdup("aaaaaaaaaaaaaaa");
-    printf("honke: [%s]\n", dup1);
+    printf(" honke: [%s]\n", dup1);
     free(dup1);
     dup2 = ft_strdup("aaaaaaaaaaaaaaa");
     printf("zisaku: [%s]\n", dup2);
     free(dup2);
 
     dup1 = strdup("");
-    printf("honke: [%s]\n", dup1);
+    printf(" honke: [%s]\n", dup1);
     free(dup1);
     dup2 = ft_strdup("");
     printf("zisaku: [%s]\n", dup2);
--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -3,23 +3,25 @@ section .text
 
 _ft_strcmp:
     ; int   ft_strcmp(const char *s1, const char *s2)   s1 = rdi, s2 = rsi
+    xor rax, rax
     xor rdx, rdx
     xor rcx, rcx                            ; i = 0
     jmp .loop
 
 .loop:
     mov dl, BYTE[rdi + rcx]
-    cmp dl, BYTE[rsi + rcx]                 ; if (s1[i] - s2[i])
-    jne _cmp                                ; s1[i] != s2[i]
+    mov al, BYTE[rsi + rcx]
     cmp dl, 0                               ; if (s1[i] == '\0')
     je  _cmp
-    cmp BYTE[rsi + rcx], 0                  ; if (s2[i] == '\0')
+    cmp al, 0                  ; if (s2[i] == '\0')
     je  _cmp
+    cmp rdx, rax                            ; if (s1[i] - s2[i])
+    jne _cmp                                ; s1[i] != s2[i]
     inc rcx                                 ; i++
     jmp .loop
 
 _cmp:
-    cmp dl, BYTE[rsi + rcx]
+    cmp rdx, rax
     jg  _greater
     jl  _less
     je  _equal
